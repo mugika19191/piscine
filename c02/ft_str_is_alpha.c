f9f70d6d07e9d9767d276bb4938ecf386cd698c9
Mugika <61477854+mugika19191@users.noreply.github.com>
2024-10-21 19:44:14 +0200

    Create ft_str_is_alpha.c

---
+++ c02/ft_str_is_alpha.c
@@ -0,0 +1,12 @@
+int ft_str_is_alpha(char *str)
+{
+    if (!*str)
+        return 0;
+    while(*str)
+    {
+        if (!((*str >= 'A' && *str <= 'Z') ||  (*str >= 'a' && *str <= 'z')))
+            return 0;
+        str++;
+    }
+    return 1;
+}
