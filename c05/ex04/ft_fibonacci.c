0b0b2c8898c879e8d92bbb3616ad7357b6df802a
Mugika <61477854+mugika19191@users.noreply.github.com>
2024-11-05 23:30:14 +0100

    Update ft_fibonacci.c

--- c05/ex04/ft_fibonacci.c
+++ c05/ex04/ft_fibonacci.c
@@ -14,7 +14,9 @@ int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	if (index <= 2)
-		return (index);
-	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	if (index == 0)
+		return (0);
+	if (index == 1 || index == 2)
+		return (1);
+	return ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
 }
