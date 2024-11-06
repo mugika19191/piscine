9e682113aa69869f3b96326d462c9fdde017064f
Mugika <61477854+mugika19191@users.noreply.github.com>
2024-11-06 10:02:23 +0100

    Update ft_iterative_power.c

--- c05/ex02/ft_iterative_power.c
+++ c05/ex02/ft_iterative_power.c
@@ -17,9 +17,9 @@ int	ft_iterative_power(int nb, int power)
 
 	i = 1;
 	temp = nb;
-	if ((nb == 0 && power !=0) || power < 0)
+	if (power < 0)
         	return (0);
-	if ((power == 0) || (( nb < 0) && (power == 0)))
+	if (power == 0)
 		return (1);
 	while (i < power)
 	{
